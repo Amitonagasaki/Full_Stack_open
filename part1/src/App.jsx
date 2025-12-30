8c01e7528394213935321ebc9ae4c962ce4238e9
amit prasad <amitlighting13@gmail.com>
2025-12-30 20:47:48 +0530

    1.6: unicafe step 1 exercise solved

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,65 +1,26 @@
-const Part =(props)=>{
-  return(
-    <div>
-      <p>{props.part} {props.exercise}</p>
-    </div>
-  )
-}
-
-const Header = (props)=>{
-  console.log(props)
-  return(
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  )
-}
-
-const Content =(props)=>{
-  console.log(props);
-  return(
-    <div>
-      <Part part = {props.parts[0].name} exercise={props.parts[0].exercises}/>
-       <Part part = {props.parts[1].name} exercise={props.parts[1].exercises}/>
-        <Part part = {props.parts[2].name} exercise={props.parts[2].exercises}/>
-    </div>
-  )
-}
-const Total = (props)=>{
-  return(
-    <div>
-      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises +props.parts[2].exercises}</p>
-    </div>  
-  )
-}
+import { useState } from 'react'
 
 const App = () => {
- const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+
+
   return (
-    <>
-    
     <div>
-       <Header course={course.name}/>
-        <Content parts={course.parts}/>
-       <Total parts={course.parts}/> 
+ 
+        <p>give feedback</p>
+         
+         <button onClick={()=>setGood(good+1)} >good</button>
+         <button onClick={()=>setNeutral(neutral+1)}>neutral</button>
+         <button onClick={()=>setBad(bad+1)}>bad</button>
+       <p>statistics</p>
+       <li>good {good}</li>
+       <li>neutral {neutral}</li>
+       <li>bad {bad}</li>
     </div>
-    </>
   )
 }
 
