bef44be4315bbce7bfd644176344aba6252039aa
amit prasad <amitlighting13@gmail.com>
2026-02-12 03:40:53 +0530

    solution for 2.2 exercise

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,14 +1,19 @@
 
 const Course=({course})=>{
+   const total = course.parts.reduce((sum,part)=>sum+part.exercises,0);
  return(
   <div>
     <Header courseName={course.name}/>
     <Content parts={course.parts}/>
-    
+    <Total total={total}/>
   </div>
  )
 }
-
+const Total =({total})=>{
+return(
+  <p>Total of {total} exercises</p>
+)
+}
 const Header=({courseName})=>{
 return(
   <h1>{courseName}</h1>
