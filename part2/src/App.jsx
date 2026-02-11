2eb620cfb57e8b14f2c0ca46b67c5bcae60b2358
amit prasad <amitlighting13@gmail.com>
2026-02-12 04:26:41 +0530

    solution for 2.5 exercise

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,39 +1,4 @@
-
-const Course=({course})=>{
-   const total = course.parts.reduce((sum,part)=>sum+part.exercises,0);
- return(
-  <div>
-    <Header courseName={course.name}/>
-    <Content parts={course.parts}/>
-    <Total total={total}/> 
-  </div>
- )
-}
-const Total =({total})=>{
-return(
-  <p>Total of {total} exercises</p>
-)
-}
-const Header=({courseName})=>{
-return(
-  <h1>{courseName}</h1>
-)
-}
-
-const Content=({parts})=>{
-return(
-  <div>
-    {parts.map(part=>
-      <Part key={part.id} part={part}/>
-    )}
-  </div>
-)
-}
-const Part=({part})=>{
-return(
-  <p>{part.name} {part.exercises}</p>
-)
-}
+import Course from "./components/Course"
 
 const App = () => {
  const courses = [
---
+++ part2/src/components/Total.jsx
@@ -0,0 +1,6 @@
+const Total =({total})=>{
+return(
+  <p>Total of {total} exercises</p>
+)
+}
+export default Total
